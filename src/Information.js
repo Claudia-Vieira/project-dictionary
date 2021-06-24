09f09331c931e3c9c4e0dd65bdcf281086b787ad
Claudia Vieira <clau_f.v@hotmail.com>
2021-06-24 19:26:38 +0100

    get part of Speech and meanings

--- src/Information.js
+++ src/Information.js
@@ -5,7 +5,15 @@ export default function Information(props){
       return(
         <div>
         <h2>{props.information.word}</h2>
-        
+        {props.information.meanings.map(function(meaning, index){
+            return(
+                <div key={index}>
+                    <h4>{meaning.partOfSpeech}</h4>
+                    <p>{meaning.definitions[0].definition}</p>
+                   {console.log(meaning)}
+                </div>
+            )
+        })}
         </div>)
        } 
 
